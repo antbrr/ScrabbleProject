d513bbc812b524b1b254b2c2180d22938899ba3d
antbrr <antbr@itu.dk>
2022-04-06 10:27:46 +0200

    Made some changes

---
+++ ScrabbleTemplate/ScrabbleBot/Dictionary.fs
@@ -0,0 +1,12 @@
+﻿module internal Dictionary
+
+type Dictionary = Set<string>
+
+let empty (_:unit) : Dictionary = Set.empty
+
+let insert word dict : Dictionary = Set.add word dict
+
+let lookup (word:string) dict : bool = Set.contains word dict
+
+
+
---
+++ ScrabbleTemplate/ScrabbleBot/Dictionary.fsi
@@ -0,0 +1,7 @@
+﻿module internal Dictionary
+
+type Dictionary = Set<string>
+    val empty : unit -> Dictionary
+    val insert : string -> Dictionary -> Dictionary
+    val lookup : string -> Dictionary -> bool
+
